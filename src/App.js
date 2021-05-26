f22a4303c82803170b7c6d4eb0544c728e524342
Michael Liendo <mtliendo@gmail.com>
2021-05-26 13:23:33 -0500

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { SketchPicker } from 'react-color'
+import './App.css'
+import LightBulb from './lightbulb'
+import axios from 'axios'
+const TOKEN = 'c7c74e9b14ff949bec26845144dc811ca5e3091dfd1bfce9c27721bbfd5f4786'
+const LIGHT_ID = 'd073d559bfdb'
 
 function App() {
+  const [bulbColor, setBulbColor] = useState('#FFD517')
+  const [color, setColor] = useState('#FFD517')
+
+  const handleColorChange = (color) => {
+    setColor(color.hex)
+  }
+
+  const handleButtonClick = async () => {
+    setBulbColor(color)
+    axios({
+      method: 'put',
+      url: `https://api.lifx.com/v1/lights/id:${LIGHT_ID}/state`,
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      data: {
+        color: color,
+      },
+    }).then((data) => {
+      console.log({ success: 'light changed!', data })
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MainContainer>
+          <LightBulb bulbColor={bulbColor} />
+          <SketchPicker color={color} onChange={handleColorChange} />
+        </MainContainer>
+        <button className="button" onClick={handleButtonClick}>
+          Change Light
+        </button>
       </header>
     </div>
-  );
+  )
 }
+const MainContainer = ({ children }) => (
+  <div
+    style={{
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+    }}
+  >
+    {children}
+  </div>
+)
 
-export default App;
+export default App
