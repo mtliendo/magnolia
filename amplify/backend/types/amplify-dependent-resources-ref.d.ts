f892c3798e8dbbe0d6b7264e84e555ecb1f0350c
Michael Liendo <mtliendo@amazon.com>
2021-11-18 16:12:24 -0600

    update with issue

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lightshowd4e81427": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "function": {
+        "lightshowfunc": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "lightshowapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -2,58 +2,47 @@ import { useState } from 'react'
 import { SketchPicker } from 'react-color'
 import './App.css'
 import LightBulb from './lightbulb'
-import axios from 'axios'
-const TOKEN = process.env.REACT_APP_LIGHT_TOKEN
-const LIGHT_ID = process.env.REACT_APP_TOKEN_ID
-console.log('the env vars', { TOKEN, LIGHT_ID })
-function App() {
-  const [bulbColor, setBulbColor] = useState('#FFD517')
-  const [color, setColor] = useState('#FFD517')
-  // the quick brown fox jumped over the lazy dog
-  const handleColorChange = (color) => {
-    setColor(color.hex)
-  }
-  // the quick brown jumped over the lazy dog
-  const handleButtonClick = async () => {
-    setBulbColor(color)
-    axios({
-      method: 'put',
-      url: `https://api.lifx.com/v1/lights/id:${LIGHT_ID}/state`,
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-      data: {
-        color: color,
-      },
-    }).then((data) => {
-      console.log({ success: 'light changed!', data })
-    })
-  }
+import API from 'aws-amplify'
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <MainContainer>
-          <LightBulb bulbColor={bulbColor} />
-          <SketchPicker color={color} onChange={handleColorChange} />
-        </MainContainer>
-        <button className="button" onClick={handleButtonClick}>
-          Change Light
-        </button>
-      </header>
-    </div>
-  )
+function App() {
+	const [bulbColor, setBulbColor] = useState('#FFD517')
+	const [color, setColor] = useState('#FFD517')
+
+	const handleColorChange = (color) => {
+		setColor(color.hex)
+	}
+
+	const handleButtonClick = async () => {
+		setBulbColor(color)
+		await API.post('lightshow', '/update-light', {
+			body: { color },
+		})
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<MainContainer>
+					<LightBulb bulbColor={bulbColor} />
+					<SketchPicker color={color} onChange={handleColorChange} />
+				</MainContainer>
+				<button className="button" onClick={handleButtonClick}>
+					Change Light
+				</button>
+			</header>
+		</div>
+	)
 }
 const MainContainer = ({ children }) => (
-  <div
-    style={{
-      width: '100vw',
-      display: 'flex',
-      justifyContent: 'center',
-    }}
-  >
-    {children}
-  </div>
+	<div
+		style={{
+			width: '100vw',
+			display: 'flex',
+			justifyContent: 'center',
+		}}
+	>
+		{children}
+	</div>
 )
 
 export default App
